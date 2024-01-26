3b8c003b4afa6c2aeff63d18a82c517509746490
LoveBroman <lo0773br-s@student.lu.se>
2024-01-26 11:58:44 +0100

    LU_factorize works now

--- LU_decomposition.py
+++ LU_decomposition.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-#A helper function that given a specific index and a matrix gives a
-#The scalar product of all the elemnts to the right
+
+# A helper function that given a specific index and a matrix gives a
+# The scalar product of all the elemnts to the right
 def back_scalar(U, x, i):
-    return U[i, i + 1:] @ x[i+1:]
+    return U[i, i + 1:] @ x[i + 1:]
 
 
 def back_sub(U, b):
@@ -16,8 +17,10 @@ def back_sub(U, b):
             x[i] = (b[i] - back_scalar(U, x, i)) / U[i, i]
     return x
 
+
 def forward_scalar(L, x, i):
     return L[i, :i] @ x[:i]
+
 
 def forward_sub(L, b):
     assert L.shape[0] == L.shape[1] == len(b)
@@ -30,12 +33,50 @@ def forward_sub(L, b):
     return x
 
 
-U = np.array([[1, 2],
-              [0, 1]])
-b = np.array([6, 5])
-x = back_sub(U, b)
-x2 = np.linalg.solve(U, b)
+def gauss_elim(A, b):
+    assert A.shape[0] == A.shape[1] == len(b)
+    U = A.copy()
+    c = b.copy()
+
+    for i, row in enumerate(U):
+        for j, rowj in list(enumerate(U))[i + 1:]:
+            fac = U[j, i] / U[i, i]
+            U[j] = U[j] - fac * U[i]
+            c[j] = c[j] - fac * c[i]
+    return U, c
 
 
-print(x)
-print(x2)
+def LU_factorize(A):
+    assert A.shape[0] == A.shape[1]
+    U = A.copy()
+    L = np.eye(A.shape[0])
+
+    for i, row in enumerate(U):
+        for j, rowj in list(enumerate(U))[i + 1:]:
+            fac = U[j, i] / U[i, i]
+            U[j] = U[j] - fac * U[i]
+            L[j, i] = fac
+    return L, U
+
+def solve(A, b):
+    U, c = gauss_elim(A, b)
+    return back_sub(U, c)
+
+def LU_solve(A, b):
+    L, U = LU_factorize(A)
+    y = forward_sub(L, b)
+    x = back_sub(U, y)
+    return x
+
+
+
+A = np.array([[4, 2, 0],
+             [4, 4, 2],
+             [2, 2, 3]])
+b = np.array([1, 2, 3])
+
+L, U = LU_factorize(A)
+
+
+print(solve(A, b))
+
--- test_lu.py
+++ test_lu.py
@@ -14,11 +14,43 @@ class TestLU(unittest.TestCase):
         self.assertTrue(np.allclose(x, x1))
 
     def test_forward_sub(self):
-        n = 5
+        n = 50
         A = np.tril(np.random.rand(n,n))
         b = np.random.rand(n)
         x = forward_sub(A, b)
         x1 = np.linalg.solve(A, b)
         self.assertTrue(np.allclose(x, x1))
+
+    def test_solve_standard(self):
+        A1 = np.array([[3, -0.5, 0.6],
+                       [4.7, 2, 2.3],
+                       [.1, -5, 9.1]])
+
+        b1 = np.array([4.57, 8.14, 46.76])
+        A2 = np.array([[1, 1, 1],
+                       [1, -1, 2],
+                       [3, 1, 4]])
+        b2 = np.array([1, 1, 1])
+
+        x1 = solve(A1, b1)
+        x1t = np.linalg.solve(A1, b1)
+        self.assertTrue(np.allclose(x1, x1t))
+        x2 = solve(A2, b2)
+        x2t = np.linalg.solve(A2, b2)
+        print(x2)
+        print(x2t)
+        #self.assertTrue(np.allclose(x2, x2t))
+        #x2 Does not work because of singular matrix
+
+    def test_solve(self):
+        n = 500
+        A = np.random.rand(n,n)
+        b = np.random.rand(n)
+        x = solve(A, b)
+        x1 = np.linalg.solve(A, b)
+        self.assertTrue(np.allclose(x, x1))
+
+
+
 
 
