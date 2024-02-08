92a1878390d1815f7959e3cb9913384c0864f465
LoveBroman <lo0773br-s@student.lu.se>
2024-02-08 09:29:12 +0100

    Jacobian done

--- multi_variate_newton.py
+++ multi_variate_newton.py
@@ -8,22 +8,17 @@ def partial_diff(f, var, eps=10**-11):
         k_copy.pop(var)
 
         pf = partial(f, **{**k_copy, var: y + eps})
-        mf = partial(f, **{**k_copy, var: y - eps })
+        mf = partial(f, **{**k_copy, var: y - eps})
 
         return (pf() - mf()) / (2 * eps)
     return fp
 
-def f(**kwargs):
-    x = kwargs["x"]
-    y = kwargs["y"]
-    z = kwargs["z"]
-    return x + 4 * y**2 + 3*z*y
+def jacobian(vars, fs, values):
+    jac = np.zeros((fs, vars))
+    for i, f in enumerate(fs):
+        for j, var in enumerate(vars):
+            df = partial_diff(f, var)
+            jac[i, j] = df(**values)
+    return jac
 
-fp = partial_diff(f, "y")
-print(fp(x=1, y=2, z=3))
-
-def jacobian(numvars, *fs):
-    jac = np.zeros((fs, numvars))
-    for f in fs:
-        for var in range(numvars):
-            pass
+# def multi_variate_newton(fs, x0, n=100):
