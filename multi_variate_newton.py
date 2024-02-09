937483e1c50fb1cc83a403f9b12102d2cf853877
LoveBroman <lo0773br-s@student.lu.se>
2024-02-09 09:19:17 +0100

    Added more efficent way to calculate newton + exact diff calculations

--- multi_variate_newton.py
+++ multi_variate_newton.py
@@ -1,5 +1,6 @@
 import numpy as np
 from functools import partial
+import matplotlib.pyplot as plt
 
 def partial_diff(f, var, eps=10**-11):
     def fp(**kwargs):
@@ -7,10 +8,10 @@ def partial_diff(f, var, eps=10**-11):
         k_copy = kwargs.copy()
         k_copy.pop(var)
 
-        pf = partial(f, **{**k_copy, var: y + eps})
-        mf = partial(f, **{**k_copy, var: y - eps})
+        pf = f(**{**k_copy, var: y + eps})
+        mf = f(**{**k_copy, var: y - eps})
 
-        return (pf() - mf()) / (2 * eps)
+        return (pf - mf) / (2 * eps)
     return fp
 
 def jacobian(fs, vars, values):
@@ -22,7 +23,7 @@ def jacobian(fs, vars, values):
     return jac
 
 def multi_apply(fs, values):
-    arr = np.zeros_like(fs)
+    arr = np.zeros_like(fs, dtype="float64")
     for i, f in enumerate(fs):
         arr[i] = f(**values)
     return arr
@@ -45,27 +46,65 @@ def multi_variate_newton(fs, vars, x0, n=100, roots=None):
     x = dict(zip(vars, x))
     return x
 
+
+def exact_jacobian(x_dict):
+    x1 = x_dict["x1"]
+    x2 = x_dict["x2"]
+    x3 = x_dict["x3"]
+    n = len(x_dict)
+    jac = np.zeros((n,n), dtype="float64")
+    jac[0,:] = [2*(x1-2), 2*x2, -1]
+    jac[1,:] = [x2 ** 2 -1, 2*x1*x2 + x3 - 3, x2]
+    jac[2,:] = [x3**2 + x2, x3**2 + x1, 2*x3 * (x1 + x2) -3]
+    return jac
+
+#avoids inverses
+def multi_variate_newton2(fs, vars, x0, n=100, roots=None):
+    x = x0
+    for i in range(n):
+        x_dict = dict(zip(vars, x))
+        Da = exact_jacobian(x_dict)
+        dx = np.linalg.solve(Da,- multi_apply(fs, x_dict))
+        xnew = x + dx
+        if roots is not None:
+            ek = err(x, roots)
+            ekp = err(xnew, roots)
+            print(f"Error after {i} iteration", ekp / ek)
+        x = xnew
+    x = dict(zip(vars, x))
+    return x
+
 def f1(**kwargs):
     x1 = kwargs["x1"]
     x2 = kwargs["x2"]
     x3 = kwargs["x3"]
-    return x1 **2 -2*x1 + x2 **2 - x3 + 1
+    return x1**2 - 2*x1 + x2**2 - x3 + 1
 
 def f2(**kwargs):
     x1 = kwargs["x1"]
     x2 = kwargs["x2"]
     x3 = kwargs["x3"]
-    return x1 *x2 **2 - x1 -3*x2 + x2*x3 +2
+    return x1 * x2**2 - x1 -3*x2 + x2*x3 +2
 
 def f3(**kwargs):
     x1 = kwargs["x1"]
     x2 = kwargs["x2"]
     x3 = kwargs["x3"]
-    return x1*x3**2 - 3 * x3 + x2*x3**2 + x1*x2
+    return x1*x3**2 - 3*x3 + x2*x3**2 + x1*x2
+
+# def plot_jacobian(f):
+#
+#     rows, cols = Da.shape
+#     fig, axs = plt.subplots(rows, cols, figsize=(15, 15))
+#     for i, row in Da:
+#         for
 
 
 fs = [f1, f2, f3]
 vars = ["x1", "x2", "x3"]
 
-x100 = multi_variate_newton(fs, vars, np.arange(3) +1, roots=np.ones(3))
+x100 = multi_variate_newton(fs, vars, np.arange(3) + 1, roots=np.ones(3))
+y100 = multi_variate_newton2(fs, vars, np.arange(3) + 1, roots=np.ones(3))
+
 print(x100)
+print(y100)
